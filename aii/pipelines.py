4124e0e5e12f7091b7318ac475210f74ad7cd1fe
Dongdong Du <du.dongdong@outlook.com>
2019-12-20 11:32:50 +0800

    init v1.0

---
+++ aii/pipelines.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+from scrapy.pipelines.files import FilesPipeline
+from os.path import join
+import scrapy
+
+
+class AiiFilesPipeline(FilesPipeline):
+    def get_media_requests(self, item, info):
+        for file_url in item['file_urls']:
+            yield scrapy.Request(file_url, meta={'itm': item}, callback=self.file_path)
+
+    def file_path(self, request, response=None, info=None):
+        itm = request.meta['itm']
+        file_name = itm['title'] + '-' + itm['date'] + '.pdf'
+
+        # remove special file name characters
+        file_name.replace("/", "-")
+        file_name.replace("'", "")
+        file_name.replace('"', '')
+
+        return file_name
---
+++ aii/spiders/files.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.selector import Selector
+from aii.items import AiiItem
+
+
+class FilesSpider(scrapy.Spider):
+    name = 'files'
+    allowed_domains = ['aii-alliance.org']
+
+    # start_urls = ['http://aii-alliance.org/index.php?m=content&c=index&a=lists&catid=23&page=1']
+
+    def start_requests(self):
+
+        # white papers
+        page = 2
+        for i in range(1, page + 1):
+            url = 'http://www.aii-alliance.org/index.php?m=content&c=index&a=lists&catid=26&page=' + str(i)
+            yield scrapy.Request(url=url, callback=self.parse_list)
+
+            # # Publications
+            # page = 2
+            # url = 'http://www.aii-alliance.org/index.php?m=content&c=index&a=lists&catid=23&page=' + str(i)
+            # for i in range(1, page + 1):
+            #     # url = 'http://www.aii-alliance.org/index.php?m=content&c=index&a=lists&catid=23&page=' + str(i)
+            #     yield scrapy.Request(url=url, callback=self.parse_list)
+
+    def parse_list(self, response):
+        body = response.body
+
+        list = Selector(text=body).css('ul.Meeting_box a').extract()
+
+        for li in list:
+            itm = AiiItem()
+            link = Selector(text=li).css('a').xpath('@href').extract()
+            date_list = Selector(text=li).xpath('//div/text()').extract()
+
+            if (len(date_list) * len(link)) > 0:
+                itm['link'] = link[0]
+                itm['date'] = date_list[-1][1:]
+                yield scrapy.Request(url=itm['link'], meta={'itm': itm}, callback=self.parse)
+
+    def parse(self, response):
+        itm = response.meta['itm']
+
+        body = response.body
+        itm['title'] = Selector(text=body).css('div.news-content h2').xpath('text()').extract()[0]
+        itm['file_urls'] = ['http://www.aii-alliance.org/' + \
+                            Selector(text=body).css('div.news-content a').xpath('@href').extract()[0]]
+        yield itm
