4de49b0e3e0229d07210bd3f501e5b7d508ce782
Dongdong Du <du.dongdong@outlook.com>
2020-07-08 22:25:56 +0800

    added 3 additional spiders

--- aii/spiders/publications.py
+++ aii/spiders/publications.py
@@ -2,28 +2,18 @@
 import scrapy
 from scrapy.selector import Selector
 from aii.items import AiiItem
+import hashlib
 
 
-class FilesSpider(scrapy.Spider):
-    name = 'files'
+class PublicationsSpider(scrapy.Spider):
+    name = 'publications'
     allowed_domains = ['aii-alliance.org']
 
-    # start_urls = ['http://aii-alliance.org/index.php?m=content&c=index&a=lists&catid=23&page=1']
+    start_urls = ['http://www.aii-alliance.org/index.php?m=content&c=index&a=lists&catid=24&page=1']
 
     def start_requests(self):
-
-        # white papers
-        page = 2
-        for i in range(1, page + 1):
-            url = 'http://www.aii-alliance.org/index.php?m=content&c=index&a=lists&catid=23&page=' + str(i)
+        for url in self.start_urls:
             yield scrapy.Request(url=url, callback=self.parse_list)
-
-            # # Publications
-            # page = 2
-            # url = 'http://www.aii-alliance.org/index.php?m=content&c=index&a=lists&catid=23&page=' + str(i)
-            # for i in range(1, page + 1):
-            #     # url = 'http://www.aii-alliance.org/index.php?m=content&c=index&a=lists&catid=23&page=' + str(i)
-            #     yield scrapy.Request(url=url, callback=self.parse_list)
 
     def parse_list(self, response):
         body = response.body
@@ -37,14 +27,27 @@ class FilesSpider(scrapy.Spider):
 
             if (len(date_list) * len(link)) > 0:
                 itm['link'] = link[0]
+                itm['url_hash'] = hashlib.md5(link[0].encode('utf-8')).hexdigest()
                 itm['date'] = date_list[-1][1:]
                 yield scrapy.Request(url=itm['link'], meta={'itm': itm}, callback=self.parse)
 
+        next_page_url = Selector(text=body).css('#pages a:last-child').xpath('@href').extract()
+        if len(next_page_url) > 0:
+            next_page_url = 'http://www.aii-alliance.org/' + next_page_url[0]
+            if next_page_url != response.request.url:
+                yield scrapy.Request(url=next_page_url, callback=self.parse_list)
+
     def parse(self, response):
         itm = response.meta['itm']
-
         body = response.body
+
         itm['title'] = Selector(text=body).css('div.news-content h2').xpath('text()').extract()[0]
-        itm['file_urls'] = ['http://www.aii-alliance.org/' + \
-                            Selector(text=body).css('div.news-content > a').xpath('@href').extract()[0]]
+
+        file_url = Selector(text=body).css('div.news-content > a').xpath('@href').extract()
+
+        if len(file_url) > 0:
+            itm['file_urls'] = ['http://www.aii-alliance.org/' + file_url[0]]
+        else:
+            itm['file_urls'] = []
+
         yield itm
